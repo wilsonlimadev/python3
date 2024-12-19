4943deffdc6072910bd8b5b373e52c14d3fed8a0
Wilson Lima <wilsonlima.dev@gmail.comls>
2024-12-19 17:06:06 -0300

    Adicionado aula3

---
+++ aula3.py
@@ -0,0 +1,18 @@
+
+ # Python = Linguagem de programação
+ # Tipo de tipagem = Dinâmica / Forte
+ # str -> string -> texto
+ # Strings são textos que estão dentro de aspas
+
+
+print(1234)
+
+
+# Aspas simples
+print('Wilson Lima')
+print(1, 'Wilson "Lima"')
+# Aspas duplas
+print("wilson Lima")
+print(2, "wilson 'Lima'")
+# Escape
+print(r"wilson \"Lima\"")
