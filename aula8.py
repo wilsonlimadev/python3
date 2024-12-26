90e3bb6baee97a132d70da4485b3ea8386ecf426
Wilson Lima <wilsonlima.dev@gmail.comls>
2024-12-26 10:24:14 -0300

    adicionando as aulas 8 e 9

---
+++ aula8.py
@@ -0,0 +1,13 @@
+nome = 'João'
+sobrenome = 'Lima'
+idade = 35
+ano_nascimento = 1988
+maior_de_idade = idade >= 18
+altura_metros = 1.7
+
+print('Nome: ', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade? ', maior_de_idade)
+print('Altura em metros: ', altura_metros)
---
+++ aula9.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('Adiçao', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Modulo', modulo)
